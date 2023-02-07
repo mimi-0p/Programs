51d3c475788758d0026d91ec41e86272d2cfdf3a
Manish kumar sahu <102276444+mimi-0p@users.noreply.github.com>
2023-02-08 01:34:18 +0530

    second

--- JAVA/checkPrime.java
+++ JAVA/checkPrime.java
@@ -1,29 +1,16 @@
-//write java program to check prime number
-import java.util.Scanner; 
 public class checkPrime{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int n = sc.nextInt();
-        int flag = 0;
-        for(int i = 2; i <= n/2; i++) {
-            if(n % i == 0) {
-                flag = 1;
-                break;
-                }
-                }
-                if(flag == 0) {
-                    System.out.println("Yes");
-                    }
-                    else {
-                        System.out.println("No");
-                        }
-                        }
-                        else {
-                            System.out.println("No");
-                            }
-}    
-
-
-
-
+      int num = 30;
+      boolean flag = false;
+      for(int i = 2; i <= num/2; ++i) {
+        if(num % i == 0) {
+          flag = true;
+          break;
+        }
+      }
+      if (!flag)
+        System.out.println(num + " is a prime number.");
+      else
+        System.out.println(num + " is not a prime number.");
+    }
+  }
